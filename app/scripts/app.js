9c8dfd8feb9a1bfece0a060ef11d28e5376c0b04
Billy Dallimore <billy@sngtrkr.com>
2014-01-15 17:26:42 +0000

    Added function to detect the langauge query string value and retrieve the relevant translations.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,19 @@ angular.module('askDysonWebApp', ['pascalprecht.translate'])
                 HEADLINE: 'Willkommen',
                 INTRO_TEXT: 'Das ist ziemlich genial'
             });
-        $translateProvider.preferredLanguage('en-GB');
+
+        function getUrlVars(queryString)
+        {
+            var vars = [], hash;
+            var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+            for(var i = 0; i < hashes.length; i++)
+            {
+                hash = hashes[i].split('=');
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+            return vars;
+        }
+        $translateProvider.preferredLanguage(getUrlVars()["language"]) || 'en-GB';
         $translateProvider.fallbackLanguage('en-GB');
 }]);
