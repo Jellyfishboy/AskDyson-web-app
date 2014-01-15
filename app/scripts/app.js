fe2f79d247d28fb0740ed7a8359a27361d6c5db1
Billy Dallimore <billy@sngtrkr.com>
2014-01-15 16:58:53 +0000

    Set up new translation provider with multilingual support.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,17 @@
 'use strict';
 
-angular.module('askDysonWebAppApp', []);
+angular.module('askDysonWebApp', ['pascalprecht.translate'])
+    .config(['$translateProvider', function($translateProvider) {
+
+        $translateProvider
+            .translations('en-GB', {
+                HEADLINE: 'Welcome',
+                INTRO_TEXT: 'This is rather awesome'
+            })
+            .translations('de-DE', {
+                HEADLINE: 'Willkommen',
+                INTRO_TEXT: 'Das ist ziemlich genial'
+            });
+        $translateProvider.preferredLanguage('en-GB');
+        $translateProvider.fallbackLanguage('en-GB');
+}]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-angular.module('askDysonWebAppApp')
+angular.module('askDysonWebApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
